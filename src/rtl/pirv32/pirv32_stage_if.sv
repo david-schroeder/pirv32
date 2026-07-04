d0328e82de8574ec753b83096ba399ae63b32443
David Schröder <post@schroederdavid.de>
2026-07-04 23:54:46 +0200

    refactor IF stage

--- src/rtl/pirv32/pirv32_stage_if.sv
+++ src/rtl/pirv32/pirv32_stage_if.sv
@@ -31,15 +31,53 @@ module pirv32_stage_if
     input  tl_d2h_t ibus_i
 );
 
-    logic [31:0] pc_d, pc_q;
+    /////////////
+    //         //
+    // Signals //
+    //         //
+    /////////////
+
+    logic [31:0] pc_d, pc_if;
     logic [31:0] pc_seq;
-    logic is_taken_branch;
-    logic is_first_cycle;
+    logic        is_taken_branch;
+    logic        is_first_cycle;
 
-    assign pc_seq = pc_q + 4;
+    reg   [31:0] instr_mem [8191:0];
+    logic [31:0] instr_if;
+
+    /////////////
+    //         //
+    // IF Regs //
+    //         //
+    /////////////
+
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (~rst_ni) begin
+            pc_if          <= BOOT_ADDR;
+            instr_if       <= '0;
+            is_first_cycle <= '1;
+        end else begin
+            if (ns_ready_i) begin
+                pc_if          <= pc_d;
+                instr_if       <= instr_mem[pc_d[31:2]];
+                is_first_cycle <= '0;
+            end
+        end
+    end
+
+    /////////////////
+    //             //
+    // Stage Logic //
+    //             //
+    /////////////////
+
+    assign ns_valid_o = ~is_first_cycle & ~invalidate_i;
+
+    assign pc_seq   = pc_if + 4;
     assign pc_seq_o = pc_seq;
+    assign pc_o     = pc_if;
+    assign instr_o  = instr_if;
 
-    assign pc_o = pc_q;
     assign is_taken_branch = is_branch_i && take_branch_i;
 
     always_comb begin
@@ -51,28 +89,11 @@ module pirv32_stage_if
         endcase
     end
 
+    // TODO: replace with bus-based instruction fetch system
     assign ibus_o = '{
         a_opcode: Get,
         default : '0
     };
-
-    reg [31:0] instr_mem [8191:0];
-
-    always_ff @(posedge clk_i or negedge rst_ni) begin
-        if (~rst_ni) begin
-            pc_q <= BOOT_ADDR;
-            instr_o <= '0;
-            is_first_cycle <= '1;
-        end else begin
-            if (ns_ready_i) begin
-                pc_q <= pc_d;
-                instr_o <= instr_mem[pc_d[31:2]];
-                is_first_cycle <= '0;
-            end
-        end
-    end
-
-    assign ns_valid_o = ~is_first_cycle & ~invalidate_i;
 
     `define STRINGIFY(x) `"x`"
     initial begin
